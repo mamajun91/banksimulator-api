4ed657047958e9ce6ca64df5dcc3ee9d3eb31ff1
mamajun <martinmahob@gmail.com>
2026-03-27 14:10:09 +0100

    actions pour sql

---
+++ src/main/resources/db/migration/V4__actions_transactions.sql
@@ -0,0 +1,4 @@
+INSERT INTO action (id, code, libelle) VALUES (gen_random_uuid(), 'ACTION_EFFECTUER_DEPOT', 'Effectuer un dépôt');
+INSERT INTO action (id, code, libelle) VALUES (gen_random_uuid(), 'ACTION_EFFECTUER_RETRAIT', 'Effectuer un retrait');
+INSERT INTO action (id, code, libelle) VALUES (gen_random_uuid(), 'ACTION_EFFECTUER_VIREMENT', 'Effectuer un virement');
+INSERT INTO action (id, code, libelle) VALUES (gen_random_uuid(), 'ACTION_ANNULER_TRANSACTION', 'Annuler une transaction');
